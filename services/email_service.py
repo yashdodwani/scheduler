e2dbc33fecf1ee2387ac4bb75a67cbd48cd663a5
yashdodwani <yashdodwani64@gmail.com>
2025-10-01 23:57:20 +0530

    added sendgrid

--- services/email_service.py
+++ services/email_service.py
@@ -1,21 +1,20 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from config import EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASSWORD
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from config import EMAIL_USER
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
 async def send_email(to_email: str, subject: str, body: str):
     try:
-        msg = MIMEMultipart()
-        msg['From'] = EMAIL_USER
-        msg['To'] = to_email
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'html'))
-        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
-        server.starttls()
-        server.login(EMAIL_USER, EMAIL_PASSWORD)
-        text = msg.as_string()
-        server.sendmail(EMAIL_USER, to_email, text)
-        server.quit()
+        message = Mail(
+            from_email=EMAIL_USER,
+            to_emails=to_email,
+            subject=subject,
+            html_content=body
+        )
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        print(response.status_code)
     except Exception as e:
         print(f"Error sending email: {str(e)}")
-
