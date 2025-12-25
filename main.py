d7febcf6513a3dacc772182c5045c504ed94cc98
yashdodwani <yashdodwani64@gmail.com>
2025-12-25 21:38:09 +0530

    added origin

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ else:
         # Optional: local dev
         "http://localhost",
         "http://localhost:5173",
+        "https://scheduler-formskart.onrender.com/",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
     ]
